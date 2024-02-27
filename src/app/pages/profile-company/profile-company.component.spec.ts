926337c5fe732055da072d0e8d83b13b516397d7
projetojobdeals <116758271+projetojobdeals@users.noreply.github.com>
2024-02-27 16:28:29 -0300

    Add files via upload

---
+++ src/app/pages/profile-company/profile-company.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProfileCompanyComponent } from './profile-company.component';
+
+describe('ProfileCompanyComponent', () => {
+  let component: ProfileCompanyComponent;
+  let fixture: ComponentFixture<ProfileCompanyComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ProfileCompanyComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(ProfileCompanyComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/profile-company/profile-company.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-company',
+  templateUrl: './profile-company.component.html',
+  styleUrl: './profile-company.component.css'
+})
+export class ProfileCompanyComponent {
+
+}
